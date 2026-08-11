807feb5a644ed8cc0168e8bd12704a3565631cd7
KTS <bosszerz181@gmail.com>
2026-08-11 15:11:31 +0700

    port

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   host: process.env.DATABASE_URL ? undefined : process.env.DB_HOST || 'localhost',
   port: process.env.DATABASE_URL ? undefined : Number(process.env.DB_PORT || 5432),
-  database: process.env.DATABASE_URL ? undefined : process.env.DB_NAME || 'mock_ris',
+  database: process.env.DATABASE_URL ? undefined : process.env.DB_NAME || 'ris',
   user: process.env.DATABASE_URL ? undefined : process.env.DB_USER || 'postgres',
   password: process.env.DATABASE_URL ? undefined : process.env.DB_PASSWORD || 'postgres123'
 });
